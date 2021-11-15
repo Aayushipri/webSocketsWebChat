0905452d4c007f9ebb5bdf82832f492420b82774
Ayushipr <ayushi.priya@zupee.in>
2021-11-15 12:52:53 +0530

    client changes

--- webSocketServer/index.js
+++ webSocketServer/index.js
@@ -12,7 +12,7 @@ const clients={};
 
 const getUniqueID = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    return s4() + s4() + '-' + s4();
+    return s4() + s4() + '-' + s4() ;
   };
   
 
